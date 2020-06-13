b68621afa8b95d34e87c2de28140123ec5ccb69c
andrej199 <66886797+andrej199@users.noreply.github.com>
2020-06-13 23:19:35 +0200

    Add files via upload

---
+++ src/main/java/SILab2.java
@@ -0,0 +1,9 @@
+public class SILab2 {
+
+    public static String function (int number) {
+        if (number%2==0) {
+            return "SEC";
+        }
+
+    }
+}
---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SILab2Test {
+
+    @Test
+    public void test1() {
+        assertEquals(SILab2.function(2), "SEC");
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(SILab2.function(6), "SEC");
+    }
+}
